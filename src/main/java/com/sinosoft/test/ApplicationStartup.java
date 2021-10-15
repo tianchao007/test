52c31ea6eb406e05406e5842c34911668c02f99c
tianchao <tianchao001@sinosoft.com.cn>
2021-10-15 10:10:17 +0800

    Initial commit

---
+++ src/main/java/com/sinosoft/test/ApplicationStartup.java
@@ -0,0 +1,16 @@
+package com.sinosoft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author TC
+ * @Description TODO
+ * @Date 2021/10/15 10:00
+ */
+@SpringBootApplication
+public class ApplicationStartup {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartup.class, args);
+    }
+}
