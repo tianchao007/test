358b85e729d7bbd2e92f7843ae8b21b32046b17b
tianchao007 <62164154+tianchao007@users.noreply.github.com>
2021-10-27 21:20:50 +0800

    Update Test.java

--- src/main/java/com/sinosoft/test/testA/Test.java
+++ src/main/java/com/sinosoft/test/testA/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public void test(){
         System.out.printf("hahahaha2222222222333332222222222222222");
         System.out.printf("hahahaha2222222222333332222222222222222");
+        System.out.printf("hahahaha2222222222333332222222222222222");
     }
 }
