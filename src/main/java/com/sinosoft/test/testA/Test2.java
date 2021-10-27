6ff6d605ae1390ff9c19ead3b0b8db78c5fe3a63
tianchao <tianchao001@sinosoft.com.cn>
2021-10-27 21:23:09 +0800

    提交代码2

---
+++ src/main/java/com/sinosoft/test/testA/Test2.java
@@ -0,0 +1,12 @@
+package com.sinosoft.test.testA;
+
+/**
+ * @Author TC
+ * @Description TODO
+ * @Date 2021/10/15 10:11
+ */
+public class Test2 {
+    public void test(){
+        System.out.printf("Test2");
+    }
+}
