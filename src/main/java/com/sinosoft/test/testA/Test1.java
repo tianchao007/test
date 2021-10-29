75eada1df3a80dae2919110215f061f6c17127b9
tianchao <tianchao001@sinosoft.com.cn>
2021-10-29 19:45:05 +0800

    提交代码第一次

---
+++ src/main/java/com/sinosoft/test/testA/Test1.java
@@ -0,0 +1,12 @@
+package com.sinosoft.test.testA;
+
+/**
+ * @Author TC
+ * @Description TODO
+ * @Date 2021/10/15 10:11
+ */
+public class Test1 {
+    public void test(){
+        System.out.printf("hahahaha2222222222333332222222222222222");
+    }
+}
